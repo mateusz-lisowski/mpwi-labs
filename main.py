3ba52fabc55b6705abf13ba4684a90e9ba5da3d5
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-22 19:49:15 +0100

    Implement generating variations without repetitions

--- main.py
+++ main.py
@@ -12,9 +12,24 @@ def generate_variations_with_repeats(input_list: list, length: int) -> list[list
     return variations
 
 
+def generate_variations_without_repeats(input_list: list, length: int) -> list[list]:
+
+    if length == 0:
+        return [[]]
+
+    variations = []
+    for i, element in enumerate(input_list):
+        remaining_elements = input_list[:i] + input_list[i+1:]
+        sub_variations = generate_variations_without_repeats(remaining_elements, length - 1)
+        for sub_variation in sub_variations:
+            variations.append([element] + sub_variation)
+
+    return variations
+
+
 def main():
-    res1 = generate_variations_with_repeats([1, 2, 3], 3)
-    print(res1)
+    pass
+
 
 if __name__ == '__main__':
     main()
