8eed0ec93f58f5e2ef6c2788f5eae934f72c2373
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-22 19:45:37 +0100

    Implement generating variations with repetitions

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-def generate_all():
-    pass
+def generate_variations_with_repeats(input_list: list, length: int) -> list[list]:
+
+    if length == 0:
+        return [[]]
+
+    variations: list[list] = []
+    for element in input_list:
+        sub_variations = generate_variations_with_repeats(input_list, length - 1)
+        for sub_variation in sub_variations:
+            variations.append([element] + sub_variation)
+
+    return variations
 
 
 def main():
-    pass
-
+    res1 = generate_variations_with_repeats([1, 2, 3], 3)
+    print(res1)
 
 if __name__ == '__main__':
     main()
